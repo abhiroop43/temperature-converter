e3407783db71fc8753b90364b4bb2338135115e1
Abhiroop Santra <abhiroop.santra@gmail.com>
2023-11-19 11:18:36 +0530

    updated code after AI review

--- src/main.rs
+++ src/main.rs
@@ -6,62 +6,71 @@ fn main() {
     println!("1. Fahrenheit to Celsius");
     println!("2. Celsius to Fahrenheit");
 
-    let mut converted_temp: f64 = 0.0;
-    let mut unit: &str;
+    let mut converted_temp = 0.0;
 
     loop {
-        let mut selection = String::new();
-
-        io::stdin()
-            .read_line(&mut selection)
-            .expect("Failed to read selection");
-
-        let selection: u8 = match selection.trim().parse() {
+        let selection: u8 = match get_user_input("Select an option:").trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Invalid selection. Please enter 1 or 2.");
+                continue;
+            }
         };
 
         if selection == 1 {
             println!("You have chosen to convert Fahrenheit to Celsius.");
-            unit = "Celcius";
         } else if selection == 2 {
-            println!("You have chosen to convert Celcius to Fahrenheit");
-            unit = "Fahrenheit";
+            println!("You have chosen to convert Celsius to Fahrenheit");
         } else {
-            println!("That is not a valid option. Please use one of the following options.");
-            println!("1. Fahrenheit to Celsius");
-            println!("2. Celsius to Fahrenheit");
+            println!("Invalid option. Please enter 1 or 2.");
             continue;
         }
 
-        println!("Please enter the temperature value.");
-
-        let mut temperature = String::new();
-
-        io::stdin()
-            .read_line(&mut temperature)
-            .expect("Failed to read the temperature");
-
-        let temperature: f64 = match temperature.trim().parse() {
+        let temperature: f64 = match get_user_input("Please enter the temperature value:")
+            .trim()
+            .parse()
+        {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Invalid temperature. Please enter a valid number.");
+                continue;
+            }
         };
 
         if selection == 1 {
-            converted_temp = convert_fahrenheit_to_celcius(temperature);
+            converted_temp = convert_fahrenheit_to_celsius(temperature);
         } else if selection == 2 {
-            converted_temp = convert_celcius_to_fahrenheit(temperature);
+            converted_temp = convert_celsius_to_fahrenheit(temperature);
         }
+
+        let unit = if selection == 1 {
+            "Celsius"
+        } else {
+            "Fahrenheit"
+        };
+
+        println!(
+            "The converted temperature is {:.2} degrees {}",
+            converted_temp, unit
+        );
+
         break;
     }
-
-    println!("The converted temperature is {converted_temp} degrees {unit}");
 }
 
-fn convert_celcius_to_fahrenheit(celcius: f64) -> f64 {
-    (9.0 * celcius / 5.0) + 32.0
+fn convert_celsius_to_fahrenheit(celsius: f64) -> f64 {
+    (celsius * 9.0 / 5.0) + 32.0
 }
 
-fn convert_fahrenheit_to_celcius(fahrenheit: f64) -> f64 {
-    (fahrenheit - 32.0) * 9.0 / 5.0
+fn convert_fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * 5.0 / 9.0
+}
+
+fn get_user_input(prompt: &str) -> String {
+    println!("{}", prompt);
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input");
+    input
 }
